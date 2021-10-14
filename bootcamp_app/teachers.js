d65734b2979ad8e1ebc421db90de87a579f221e8
YBYuan007 <yuanyanbin2011@gmail.com>
2021-10-14 01:19:33 +0000

    refactor

--- bootcamp_app/teachers.js
+++ bootcamp_app/teachers.js
@@ -6,7 +6,9 @@ const pool = new Pool ({
   database:'bootcampx'
 }); 
 
-pool.query (`
+const nn =process.argv[2]; 
+
+const a = (`
 SELECT cohorts.name as cohort_name , teachers.name as teacher_name
 FROM  assistance_requests
 INNER JOIN teachers
@@ -15,15 +17,38 @@ INNER JOIN students
 ON students.id = assistance_requests.student_id 
 INNER JOIN cohorts
 ON cohorts.id = students.cohort_id
-WHERE cohorts.name = '${process.argv[2]}'
+WHERE cohorts.name = $1
 GROUP BY teachers.name, cohorts.name
 ORDER BY teachers.name
 LIMIT 5; 
 `)
+pool
+.query(a, [nn])
 .then(res=>{
   res.rows.forEach(element => {
     console.log(`${element.cohort_name}: ${element.teacher_name}`);
   })
 });
+
+
+// pool.query (`
+// SELECT cohorts.name as cohort_name , teachers.name as teacher_name
+// FROM  assistance_requests
+// INNER JOIN teachers
+// ON teachers.id = assistance_requests.teacher_id 
+// INNER JOIN students
+// ON students.id = assistance_requests.student_id 
+// INNER JOIN cohorts
+// ON cohorts.id = students.cohort_id
+// WHERE cohorts.name = '${process.argv[2]}'
+// GROUP BY teachers.name, cohorts.name
+// ORDER BY teachers.name
+// LIMIT 5; 
+// `)
+// .then(res=>{
+//   res.rows.forEach(element => {
+//     console.log(`${element.cohort_name}: ${element.teacher_name}`);
+//   })
+// });
 
 
