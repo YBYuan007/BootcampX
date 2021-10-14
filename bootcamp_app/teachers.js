4c467e66b2ae9f08c74c7663de8427f5272e0bb3
YBYuan007 <yuanyanbin2011@gmail.com>
2021-10-14 00:58:37 +0000

    added js

---
+++ bootcamp_app/teachers.js
@@ -0,0 +1,29 @@
+const {Pool} =require('pg'); 
+const pool = new Pool ({
+  user: 'vagrant', 
+  password:'123', 
+  host:'localhost', 
+  database:'bootcampx'
+}); 
+
+pool.query (`
+SELECT cohorts.name as cohort_name , teachers.name as teacher_name
+FROM  assistance_requests
+INNER JOIN teachers
+ON teachers.id = assistance_requests.teacher_id 
+INNER JOIN students
+ON students.id = assistance_requests.student_id 
+INNER JOIN cohorts
+ON cohorts.id = students.cohort_id
+WHERE cohorts.name = '${process.argv[2]}'
+GROUP BY teachers.name, cohorts.name
+ORDER BY teachers.name
+LIMIT 5; 
+`)
+.then(res=>{
+  res.rows.forEach(element => {
+    console.log(`${element.cohort_name}: ${element.teacher_name}`);
+  })
+});
+
+
